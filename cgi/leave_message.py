7b78d3349624d0bc56cda3ae43b8f79e50e7608f
Zex <top_zlynch@yahoo.com>
2014-10-10 15:27:27 +0800

    leave_message.py initiation

---
+++ cgi/leave_message.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import cgi
+
+fields = cgi.FieldStorage()
+title = "Message Box"
+
+print "Content-Type: text/html\n\n"
+print "<!DOCTYPE html>"
+print "<html>"
+
+print "<head>"
+print "<title>", title, "</title>"
+print "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
+print "<meta charset=\"UTF-8\">"
+print "</head>"
+
+print "<body>"
+
+for k in fields.keys():
+	print "<b>", k.upper(), "</b>", fields[k].value,"<br>"
+
+print "</body>"
+print "</html>"
+
