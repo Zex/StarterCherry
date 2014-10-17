5aaab86c68f24b90a01c7087764723aaa2df6f47
Zex <top_zlynch@yahoo.com>
2014-10-17 14:25:16 +0800

    flower_man with cherrypy

--- cgi-bin/flower_man.py
+++ cgi-bin/flower_man.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 
-import cgi
+def reply(kwargs = {}):
 
-fields = cgi.FieldStorage()
-title = "Flower Man"
+    title = "Flower Man"
+    ret = ''
+    
+    ret += "<!DOCTYPE html>"
+    ret += "<html>"
+    
+    ret += "<head>"
+    ret += "<title>" + title + "</title>"
+    ret += "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
+    ret += "<meta charset=\"UTF-8\">"
+    ret += "</head>"
+    
+    ret += "<body>"
+    
+    for k in kwargs.items():
+    	ret += "<b>" + str(k[0]).upper() + ': ' + str(k[1]) + "</b><br>"
+    
+    ret += "</body>"
+    ret += "</html>"
 
-print "Content-Type: text/html\n\n"
-print "<!DOCTYPE html>"
-print "<html>"
-
-print "<head>"
-print "<title>", title, "</title>"
-print "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
-print "<meta charset=\"UTF-8\">"
-print "</head>"
-
-print "<body>"
-
-for k in fields.keys():
-	print "<b>", k.upper(), "</b>", fields[k].value,"<br>"
-
-print "</body>"
-print "</html>"
-
+    return ret
