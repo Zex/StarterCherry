9a478afc4a39f2ca231a3524cd1187512e3aa643
Zex <top_zlynch@yahoo.com>
2014-10-17 14:29:25 +0800

    ranseq with cherrypy

--- cgi-bin/ranseq.py
+++ cgi-bin/ranseq.py
@@ -7,57 +7,74 @@ from random import randint#, choice
 from string import digits
 
 def ranseq(length):
-#    print "--------------------------------------------------------->"
+#    ret += "--------------------------------------------------------->"
 #    ancient_a = [int(choice(digits)) for i in xrange(int(length))]
 #    ancient_b = [int(choice(digits)) for i in xrange(int(length))]
+    ret = "<table>"
+
     ancient_a = [randint(1, 1000) for i in xrange(int(length))]
     ancient_b = [randint(1, 1000) for i in xrange(int(length))]
 
-    print "<label>Ancient a: </label><p><span contenteditable=\"false\">", ancient_a, "</span></p>"
-    print "<label>Ancient b: </label><p><span contenteditable=\"false\">", ancient_b, "</span></p>"
+    ret += "<tr>"
+    ret += "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>Ancient b: </td><td><span contenteditable=\"false\">" + str(ancient_b) + "</span></td>"
+    ret += "</tr>"
 
     ancient_a.extend(ancient_b)
     ancient_a.sort()
 
-    print "<label>Ancient a: </label><p><span contenteditable=\"false\">", ancient_a, "</span></p>"
+    ret += "<tr>"
+    ret += "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
+    ret += "</tr>"
 
     child_min = ancient_a[:(len(ancient_a)-2)/2]
     child_max = ancient_a[(len(ancient_a)-2)/2:-2]
 
-    print "<label>child_min: </label><p><span contenteditable=\"false\">", child_min, "</span></p>"
-    print "<label>child_max: </label><p><span contenteditable=\"false\">", child_max, "</span></p>"
+    ret += "<tr>"
+    ret += "<td>child_min: </td><td><span contenteditable=\"false\">" + str(child_min) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>child_max: </td><td><span contenteditable=\"false\">" + str(child_max) + "</span></td>"
+    ret += "</tr>"
 
     child_min.append(ancient_a[-1])
     child_max.append(ancient_a[-2])
 
-    print "<label>child_min sum: </label><p><span contenteditable=\"false\">", reduce(lambda x,y:x+y, child_min), "</span></p>"
-    print "<label>child_max sum: </label><p><span contenteditable=\"false\">", reduce(lambda x,y:x+y, child_max), "</span></p>"
-#    print "---------------------------------------------------------+"
+    ret += "<tr>"
+    ret += "<td>child_min sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_min)) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>child_max sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_max)) + "</span></td>"
+    ret += "</tr>"
+#    ret += "---------------------------------------------------------+"
+    ret += "</table>"
+
+    return ret
 
 def reply():
-    import cgi
-    title = "Random Seq"
-    
-    print "Content-Type: text/html\n\n"
-    print "<!DOCTYPE html>"
-    print "<html>"
-    
-    print "<head>"
-    print "<title>", title, "</title>"
-    print "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
-    print "<meta charset=\"UTF-8\">"
-    print "</head>"
-    
-    print "<body>"
-    
-    print "<table>"
-    for i in [100, 10, 1000, 999, 3]:
-        print "<tr><td>"
-        ranseq(i)
-        print "</td></tr>"
-    print "</table>"
-    
-    print "</body>"
-    print "</html>"
 
-reply()
+    title = "Random Seq"
+    ret = ''
+ 
+    ret += "<!DOCTYPE html>"
+    ret += "<html>"
+    
+    ret += "<head>"
+    ret += "<title>" + title + "</title>"
+    ret += "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
+    ret += "<meta charset=\"UTF-8\">"
+    ret += "</head>"
+    
+    ret += "<body>"
+    
+    for i in [100, 10, 1000, 999, 3]:
+        ret += "<h2>" + "Ranseq with " + str(i) + "</h2>"
+        ret += ranseq(i)
+    
+    ret += "</body>"
+    ret += "</html>"
+
+    return ret
+
