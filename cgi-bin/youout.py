d92d8ab2e327ea418c10858db7544ab870d963d6
Zex <top_zlynch@yahoo.com>
2014-10-17 14:31:54 +0800

    youout with cherrypy

--- cgi-bin/youout.py
+++ cgi-bin/youout.py
@@ -4,8 +4,9 @@
 # Solution to the YouOut game
 #
 # Author: zex <top_zlynch@yahoo.com>
-
-import cgi
+#
+# http://localhost:8080/youout?total_player=15&unlucky_n=3
+# http://localhost:8080/youout?total_player=710&unlucky_n=43
 
 def youout(total, n):
 
@@ -20,47 +21,47 @@ def youout(total, n):
 
     return str(a)
 
-if __name__ == '__main__':
+def reply(kwargs = {}):
 
-    fields = cgi.FieldStorage()
     title = "YouOut!"
     
-    print "Content-Type: text/html\n\n"
-    print "<!DOCTYPE html>"
-    print "<html>"
+    ret = "<!DOCTYPE html>"
+    ret += "<html>"
     
-    print "<head>"
-    print "<title>", title, "</title>"
-    print "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
-    print "<meta charset=\"UTF-8\">"
-    print "</head>"
+    ret += "<head>"
+    ret += "<title>" + title + "</title>"
+    ret += "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
+    ret += "<meta charset=\"UTF-8\">"
+    ret += "</head>"
     
-    print "<body>"
+    ret += "<body>"
     
-    print "<div>"
-    print "<label for=total_player><b>Total player: </b></label>"
-    print "<input type=text name=\"total_player\" readonly=\"true\" value=\""+fields['total_player'].value+"\"/><br>"
-    print "</div>"
-    print "<div>"
-    print "<label for=\"unlucky_n\"><b>Unlucky N: </b></label>"
-    print "<input type=text name=\"unlucky_n\" readonly=\"true\" value=\""+fields['unlucky_n'].value+"\"/><br>"
-    print "</div>"
-    print "<div>"
-    print "<label><b>Lucky Dog: </b><label>"
-    print "<label><b>", youout(int(fields['total_player'].value), int(fields['unlucky_n'].value)), "</b></label><br>"
-    print "</div>"
+    ret += "<div>"
+    ret += "<label for=total_player><b>Total player: </b></label>"
+    ret += "<input type=text name=\"total_player\" readonly=\"true\" value=\"" + kwargs['total_player'] + "\"/><br>"
+    ret += "</div>"
+    ret += "<div>"
+    ret += "<label for=\"unlucky_n\"><b>Unlucky N: </b></label>"
+    ret += "<input type=text name=\"unlucky_n\" readonly=\"true\" value=\"" + kwargs['unlucky_n'] + "\"/><br>"
+    ret += "</div>"
+    ret += "<div>"
+    ret += "<label><b>Lucky Dog: </b><label>"
+    ret += "<label><b>" + youout(int(kwargs['total_player']), int(kwargs['unlucky_n'])) + "</b></label><br>"
+    ret += "</div>"
     
-    print "</body>"
-    print "</html>"
+    ret += "</body>"
+    ret += "</html>"
+
+    return ret
 
 #if __name__ == '__main__':
 #    youout(15, 3)
-#    print ":----------------------------------"
+#    ret += ":----------------------------------"
 #    youout(15, 4)
-#    print ":----------------------------------"
+#    ret += ":----------------------------------"
 #    youout(20, 7)
-#    print ":----------------------------------"
+#    ret += ":----------------------------------"
 #    youout(177, 11)
-#    print ":----------------------------------"
+#    ret += ":----------------------------------"
 #    youout(3, 11)
 
