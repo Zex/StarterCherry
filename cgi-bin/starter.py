3800b4f668b54fc4d7cf99761b0ddc071669d3fb
Zex <top_zlynch@yahoo.com>
2014-10-24 09:58:38 -0700

    add navigator

--- cgi-bin/starter.py
+++ cgi-bin/starter.py
@@ -37,45 +37,43 @@ class Starter(object):
 
     @cherry.expose
     def ranseq(self):
-#        cherry.response.headers['Expires'] = '2s'
-#        cherry.response.headers['Server'] = 'Starter Powered by CherryPy/3.2.2'
-        return ranseq.reply(cherry.request)
+        return ranseq.reply(cherry)
 
     @cherry.expose
     def hello_baby(self, fr_name=''):
-        return hello_baby.reply(cherry.request, fr_name)
+        return hello_baby.reply(cherry, fr_name)
 
     @cherry.expose
     def user_info(self):
-        return user_info.reply(cherry.request)
+        return user_info.reply(cherry)
 
     @cherry.expose
     def plotting(self):
-        return plotting.reply(cherry.request)
+        return plotting.reply(cherry)
 
     @cherry.expose
     def icecream_box(self, **kwargs):
-        return icecream_box.reply(cherry.request, kwargs)
+        return icecream_box.reply(cherry, kwargs)
 
     @cherry.expose
     def flower_man(self, **kwargs):
-        return flower_man.reply(cherry.request, kwargs)
+        return flower_man.reply(cherry, kwargs)
 
     @cherry.expose
     def motion_trigger(self, **kwargs):
-        return motion_trigger.reply(cherry.request, kwargs)
+        return motion_trigger.reply(cherry, kwargs)
 
     @cherry.expose
     def upload_file(self, **kwargs):
-        return upload_file.reply(cherry.request, kwargs)
+        return upload_file.reply(cherry, kwargs)
 
     @cherry.expose
     def roman_number(self, **kwargs):
-        return roman_number.reply(cherry.request, kwargs)
+        return roman_number.reply(cherry, kwargs)
 
     @cherry.expose
     def leave_message(self, **kwargs):
-        return leave_message.reply(cherry.request, kwargs)
+        return leave_message.reply(cherry, kwargs)
 
     @cherry.expose
     def youout(self, **kwargs):
@@ -83,15 +81,11 @@ class Starter(object):
 
     @cherry.expose
     def whereyoulive(self, **kwargs):
-        return whereyoulive.reply(cherry.request, kwargs)
-
-#    @cherry.expose
-#    def whereyoulive_sum(self, **kwargs):
-#        return whereyoulive_sum.reply(kwargs)
+        return whereyoulive.reply(cherry, kwargs)
 
     @cherry.expose
     @cherry.tools.gzip()
-    def index(self):
+    def index(self, cont = None):
 
         ret = ''
         ret += "<!DOCTYPE html>"
@@ -105,11 +99,34 @@ class Starter(object):
         ret += "</head>"
         
         ret += "<body>"
-        ret += "<div>"
+
+#        ret += "<frameset cols=\"20%, 80%\">"
+#
+#        ret += "<frame name=\"navigator\" src=\"/default/navigator.html\">"
+#
+#        if cont == None:
+#            ret += "<frame name=\"content\" src=\"/default/content.html\">"
+#        else:
+#            ret += "<frame name=\"content\" src=\"/default/content.html#" + cont + "\">"
+#
+#        ret += "</frameset>"
+
+        ret += "<div class=\"navigator\">"
+        ret += "<a name=\"Navigator\"><ul>Navigator</ul></a>"
+        ret += "<ul>"
+        ret += "<li><a href=\"#Motions\" title=\"Motions\">Motions</a></li>"
+        ret += "<li><a href=\"#RandomSeq\" title=\"Random Seq\">Random Seq</a></li>"
+        ret += "<li><a href=\"#LeaveMessage\" title=\"Leave a Message\">Leave a Message</a></li>"
+        ret += "</ul>"
+        ret += "</div>"
+
+        ret += "<div id=\"content\">"
+        ret += "<div class=\"normal\">"
         ret += "<img src=\"/img/cattail.jpg\" height=200px>"
         ret += "</div>"
-        
-        ret += "<div>"
+
+        ret += "<div class=\"normal\">"
+        ret += "<a href=\"#Navigator\" title=\"Navigator\">Navigator</a>"
         ret += "<form action=\"hello_baby\" method=get>"
         ret += "<label><b>Action: </b></label>"
         ret += "<input type=\"text\" name=\"fr_name\"/>"
@@ -117,7 +134,8 @@ class Starter(object):
         ret += "</form>"
         ret += "</div>"
         
-        ret += "<div>"
+        ret += "<div class=\"normal\">"
+        ret += "<a href=\"#Navigator\" title=\"Navigator\">Navigator</a>"
         ret += "<form action=\"upload_file\" method=post enctype=\"multipart/form-data\">"
         ret += "<label><b>Select file: </b></label>"
         ret += "<input type=\"file\" name=\"file_name\"/>"
@@ -125,10 +143,10 @@ class Starter(object):
         ret += "</form>"
         ret += "</div>"
         
-        ret += "<table>"
+        ret += "<table class=\"normal\">"
         
-        ret += "<tr>"
-        ret += "<td>"
+        ret += "<tr class=\"normal\">"
+        ret += "<td class=\"normal\">"
         icecreams = [ "Milk", "Chocolate", "Strawberry", "Coconut", "Peanut" ]
         
         ret += "<form action=\"icecream_box\" method=post>"
@@ -140,7 +158,7 @@ class Starter(object):
         ret += "<input type=\"submit\" value=\"Done!\"/><br>"
         ret += "</form>"
         ret += "</td>"
-        ret += "<td>"
+        ret += "<td class=\"normal\">"
         
         flowers = [ "Almond Blossom", "Balsam", "Anther", "Camellia", "Azalea" ]
         
@@ -157,9 +175,10 @@ class Starter(object):
         ret += "</table>"
         
         options = [ "Go", "Walk Away", "Close", "Laught" ]
-        ret += "<div>"
+        ret += "<div class=\"normal\">"
+        ret += "<a href=\"#Navigator\" title=\"Navigator\">Navigator</a>"
         ret += "<form action=\"motion_trigger\" method=post>"
-        ret += "<label for=option><b>Motions: </b></label><br>"
+        ret += "<label for=option><a name=\"Motions\"><b>Motions</b></a>:</label><br>"
         ret += "<select name=option>"
         
         for o in options:
@@ -170,49 +189,52 @@ class Starter(object):
         ret += "</form>"
         ret += "</div>"
         
-        ret += "<div>"
+        ret += "<div class=\"normal\">"
         ret += "<label><b>Current time: </b><label>"
-        ret += "<input id=\"cur_time\" type=text rows=30 cols=100 readonly=\"true\" value=\"" + time.strftime(time.asctime()) + "\"/><br>"
+        ret += "<input id=\"cur_time\" type=\"text\" readonly=\"true\" value=\"" + time.strftime(time.asctime()) + "\"/><br>"
         ret += "</form>"
         ret += "</div>"
         
-        ret += "<div>"
+        ret += "<div class=\"normal\">"
+        ret += "<a href=\"#Navigator\" title=\"Navigator\">Navigator</a>"
         ret += "<form action=\"leave_message\" method=post>"
-        ret += "<label for=msgbox><b>Leave a Message: </b></label><br>"
+        ret += "<a name=\"LeaveMessage\"><label for=msgbox><b>Leave a Message: </b></label></a><br>"
         ret += "<textarea id=\"msgbox\" name=\"msgbox\"></textarea><br>"
         ret += "<input type=\"submit\" value=\"Submit\"/><br>"
         ret += "</form>"
         ret += "</div>"
-        
-        ret += "<table>"
-        ret += "<tr>"
-        ret += "<td>"
+       
+        ret += "<div class=\"normal\"><a href=\"#Navigator\" title=\"Navigator\">Navigator</a></div>"
+
+        ret += "<table class=\"normal\">"
+        ret += "<tr class=\"normal\">"
+        ret += "<td class=\"normal\">"
         ret += "<form action=\"user_info\" method=get>"
         ret += "<input type=\"submit\" value=\"User Info\"/><br>"
         ret += "</form>"
         ret += "</td>"
         
-        ret += "<td>"
+        ret += "<td class=\"normal\">"
         ret += "<form action=\"ranseq\" method=get>"
-        ret += "<input type=\"submit\" value=\"Random Seq\"/><br>"
+        ret += "<a name=\"RandomSeq\"><input type=\"submit\" value=\"Random Seq\"/></a><br>"
         ret += "</form>"
         ret += "</td>"
         
-        ret += "<td>"
+        ret += "<td class=\"normal\">"
         ret += "<form action=\"roman_number\" method=get>"
         ret += "<span>Arabic/Roman Number </span><input name=\"rnum\" type=\"text\"/><br>"
         ret += "</form>"
         ret += "</td>"
         
-        ret += "<td>"
+        ret += "<td class=\"normal\">"
         ret += "<form action=\"plotting\" method=post>"
         ret += "<input type=\"submit\" value=\"Plot\"/><br>"
         ret += "</form>"
         ret += "</td>"
         
         for i in xrange(1):
-            ret += "<td>"
-            ret += "<form action=\"hello_baby.py\" method=get>"
+            ret += "<td class=\"normal\">"
+            ret += "<form action=\"hello_baby\" method=get>"
             ret += "<input type=\"submit\" value=\"TOBE CONTINUE...\"/><br>"
             ret += "</form>"
             ret += "</td>"
@@ -220,6 +242,8 @@ class Starter(object):
         ret += "</tr>"
         ret += "</table>"
         
+        ret += "</div>"
+
         ret += "</body>"
         ret += "</html>"
 
@@ -231,6 +255,8 @@ if __name__ == '__main__':
         '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd()),
+            'error_page.404': '../default/Starter404.html',
+            'error_page.500': '../default/Starter500.html'
         },
         '/css': {
             'tools.staticdir.on': True,
@@ -252,10 +278,6 @@ if __name__ == '__main__':
             'tools.staticdir.on': True,
             'tools.staticdir.dir': '../default'
         },
-        'global': {
-            'error_page.404': '../default/Starter404.html',
-            'error_page.500': '../default/Starter500.html'
-        }
     }
 
     from socket import gethostname
